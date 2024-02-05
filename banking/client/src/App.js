cd02efbc63e76646a963603ae09ce14d62ae7033
Dheerajketa <dheerajketa552005@gmail.com>
2024-02-05 18:28:54 +0530

    signup and login connected to database

--- banking/client/src/App.js
+++ banking/client/src/App.js
@@ -1,16 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import Registration from './components/Registration'
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
+import Home from "./components/Home";
+import About from "./components/About";
+import Login from "./components/Login";
+import {Navigation} from "@mui/icons-material";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          This is Dheeraj nice to meet you
-        </p>
-      </header>
-    </div>
+      <>
+
+        <div>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/login" element={<Login/>} />
+            <Route path="/register" element={<Registration/>} />
+          </Routes>
+
+        </div></>
+
+
+
+
+
+
   );
 }
 
